ab084aecfcf659c604826fc955f8f59ab2fd0e6a
lasupernova <gabrilasuno@gmail.com>
2021-11-28 23:54:46 +0100

    changed layout

--- app.py
+++ app.py
@@ -6,49 +6,84 @@ import mortgage_calc
 
 
 # app initiation
-external_stylesheets = [dbc.themes.LITERA]
+external_stylesheets = [dbc.themes.SOLAR]
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 # layout
 INPUT_WIDTH = 4
 app.layout = html.Div([
-    dbc.Row([
-            dbc.Col([
-                dbc.Row([
-                    dbc.Col([dcc.Input(id='mortgage_period', type='number', placeholder="Mortgage period", min=1, max=30, step=1, value=30)], 
-                            width=INPUT_WIDTH),
-                    dbc.Col([html.Label(id='mortgage_period_text', children=["years"], title="Mortgage period")],
-                    width=3)
-                ]),
-                dbc.Row([
-                    dbc.Col([dcc.Input(id='interest_rate_yearly', type='number', placeholder="Interest rate", min=0, max=100, step=0.005, value=3.375)], 
-                            width=INPUT_WIDTH),
-                    dbc.Col([html.Label(id='interest_rate_text', children=["%"], title="Yearly Interest Rate")],
-                    width=3)
-                ]),
-                dbc.Row([
-                    dbc.Col([dcc.Input(id='downpayment', type='number', placeholder="Downpayment [%]", min=0, max=100, step=1, value=25)], 
-                            width=INPUT_WIDTH),
-                    dbc.Col([html.Label(id='downpayment_text', children=["%"], title="Downpayment")],
-                    width=3)
-                ]),
-                html.Div(dcc.Input(id='offer', type='number', placeholder="Offer amount")),
-                html.Button('Submit', id='submit-val'),
-                dbc.Row([html.Div(id='monthly_payment',
-                                children='Enter desired values and press submit')
-                        ], style={"margin": "15px", "justify":"center"})
+        dbc.Row([
+                dbc.Col([
+                    dbc.Row([
+                        dbc.Col([dcc.Input(id='mortgage_period', type='number', placeholder="Mortgage period", min=1, max=30, step=1, value=30)], 
+                                width=INPUT_WIDTH),
+                        dbc.Col([html.Label(id='mortgage_period_text', children=["years"], title="Mortgage period")],
+                        width=3)
                     ]),
-        dbc.Col(
+                    dbc.Row([
+                        dbc.Col([dcc.Input(id='interest_rate_yearly', type='number', placeholder="Interest rate", min=0, max=100, step=0.005, value=3.375)], 
+                                width=INPUT_WIDTH),
+                        dbc.Col([html.Label(id='interest_rate_text', children=["%"], title="Yearly Interest Rate")],
+                        width=3)
+                    ]),
+                    dbc.Row([
+                        dbc.Col([dcc.Input(id='downpayment', type='number', placeholder="Downpayment [%]", min=0, max=100, step=1, value=25)], 
+                                width=INPUT_WIDTH),
+                        dbc.Col([html.Label(id='downpayment_text', children=["%"], title="Downpayment")],
+                        width=3)
+                    ]),
+                    dbc.Row([
+                        dbc.Col([dcc.Input(id='offer', type='number', placeholder="Offer Amount")], 
+                                width=INPUT_WIDTH),
+                        dbc.Col([html.Label(id='offer_text', children=["$"], title="Offer Amount")],
+                        width=3)
+                    ]),
+                    html.Button('Submit', id='submit-val'),
+                    html.Hr(),
+                    dbc.Row(id='monthly_payment',
+                                    children='Enter desired values and press submit')
+                        ], width=4, style={"margin": "15px"}),
+                dbc.Col([
+                    dbc.Row([
+                        dbc.Col([dcc.Input(id='mortgage_period2', type='number', placeholder="Mortgage period", min=1, max=30, step=1, value=30)], 
+                                width=INPUT_WIDTH),
+                        dbc.Col([html.Label(id='mortgage_period_text2', children=["years"], title="Mortgage period")],
+                        width=3)
+                    ]),
+                    dbc.Row([
+                        dbc.Col([dcc.Input(id='interest_rate_yearly2', type='number', placeholder="Interest rate", min=0, max=100, step=0.005, value=3.375)], 
+                                width=INPUT_WIDTH),
+                        dbc.Col([html.Label(id='interest_rate_text2', children=["%"], title="Yearly Interest Rate")],
+                        width=3)
+                    ]),
+                    dbc.Row([
+                        dbc.Col([dcc.Input(id='downpayment2', type='number', placeholder="Downpayment [%]", min=0, max=100, step=1, value=25)], 
+                                width=INPUT_WIDTH),
+                        dbc.Col([html.Label(id='downpayment_text2', children=["%"], title="Downpayment")],
+                        width=3),
+                    ]),
+                    dbc.Row([
+                        dbc.Col([dcc.Input(id='offer2', type='number', placeholder="Offer Amount")], 
+                                width=INPUT_WIDTH),
+                        dbc.Col([html.Label(id='offer_text2', children=["$"], title="Offer Amount")],
+                        width=3)
+                    ]),
+                    html.Button('Submit', id='submit-val2'),
+                    html.Hr(),
+                    dbc.Row(id='monthly_payment2',
+                                    children='Enter desired values and press submit')
+                        ], style={"margin": "15px"}),
+        dbc.Row(
             [
             dcc.Loading(id="loading-1",
                         type="default",
                         children=[
                             html.Div(id='viz_loan_balance',
                                 children=[])
-                        ])
-            ], width="auto", style={"margin": "15px", 'width': '65vh', 'height': '90vh', "justify":"center", "align":"center"})
-        ], justify="center", align="center", className="h-50")
-    ])
+                            ])
+            ], style={"margin": "15px", "height":"auto", "justify":"center", "align":"center"})
+        ], style={"overflow":"hidden"})
+])
 
 
 # callbacks
@@ -75,9 +110,20 @@ def calculate_payment(n_clicks, mortgage_period, interest_rate, downpayment, off
         total_cost = mortgage_calc.total_cost(loan, interest_rate/12, mortgage_period)
         #total interest paid
         total_interest = total_cost - loan
-        info=[html.Div(id='monthly_pay_result', children=[f'{"Monthly payments will be:"} ${result:.2f}']),
-              html.Div(id='total_cost', children=[f'{"Total costs of mortgage:"} ${total_cost:.0f}']),
-              html.Div(id='total_interest', children=[f'Total interest paid: ${total_interest:.0f}'])]
+        #info_texts
+        info_col_width = 4
+        info=[dbc.Col([
+                dbc.Row([html.Label(id='monthly_pay_label', children=[f'Mthly. Payment'])], style={'height': '10vh'}),
+                dbc.Row([html.Label(id='monthly_pay_result', children=[f'${result:.2f}'])], style={'height': '15vh'})
+                ], width=info_col_width),
+             dbc.Col([
+                dbc.Row([html.Label(id='downpayment_label', children=[f'Downpayment'])], style={'height': '10vh'}),
+                dbc.Row([html.Label(id='downpayment_result', children=[f'${offer*(downpayment/100):.0f}'])], style={'height': '15vh', "font-size":"26"})
+                ], width=info_col_width),
+            dbc.Col([
+                dbc.Row([html.Label(id='loan_label', children=[f'Loan'])], style={'height': '10vh'}),
+                dbc.Row([html.Label(id='loan_result', children=[f'${loan:.0f}'])], style={'height': '15vh', "font-size":"26"})
+                ], width=info_col_width)]
 
         ## mortgage viz
         loan_balance = mortgage_calc.loan_balance_overview(loan, interest_rate/12, mortgage_period, yoi=list(range(1, 31)))
@@ -92,7 +138,13 @@ def calculate_payment(n_clicks, mortgage_period, interest_rate, downpayment, off
                         },
                     style={'display':'inline-block', 'width': '90vh', 'height': '90vh', 'margin':'30px'}
                 )
-        return info, loan_balance_viz
+        checkbox = dcc.Checklist(
+            options=[
+                {'label': 'Show amortization', 'value': 'show_loan_balance'}
+            ],
+            value=[]
+        )
+        return info, checkbox
 
 
 if __name__ == '__main__':
