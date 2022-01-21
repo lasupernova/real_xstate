6f8bfc050125d004ac349ca339bf7bd650faa056
lasupernova <gabrilasuno@gmail.com>
2022-01-22 00:08:41 +0100

    corrected processing of parameters for calculateMortgage calculation

--- mobile_app/lib/cashflowResultDetails._screen.dart
+++ mobile_app/lib/cashflowResultDetails._screen.dart
@@ -3,17 +3,19 @@ import 'dart:math';
 
 import 'package:mobile_app/cashflowResultTile.dart';
 
+// called from cashflowResultTile.dart
 class CfResultDetailsScreen extends StatelessWidget {
   static const routeName = "/cashflow_details";
 
   double calculateMortgage(
-    offer,
-    downpayment,
-    interest,
-    term,
+    offer, // in $
+    downpayment, //in %
+    interest, // yearly in %
+    term, // in years --> calculation itself takes months, therefore if years are given term needs to be 'translated' into months
   ) {
-    double loanAmount = offer * (downpayment / 100);
-    interest = interest / 12;
+    double loanAmount = offer * (1 - downpayment / 100);
+    interest = interest / 100 / 12; // convert to monthly interest rate
+    term = term * 12; // convert to months
     double M = loanAmount *
         (interest * pow((1 + interest), term)) /
         (pow((1 + interest), term) - 1);
@@ -31,11 +33,17 @@ class CfResultDetailsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Cashflow Details"),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () => print("EXPORTED"),
+            icon: Icon(Icons.share),
+          ),
+        ],
       ),
       body: Container(
         child: Center(
           child: Text(
-            "Term: $term, interest: $interest, offer: $offer, downpayment: $downpayment --- Monthly Payment: ${calculateMortgage(offer, downpayment, interest, term).toString()}",
+            "Term: $term, interest: ${interest * 100}%, offer: $offer, downpayment: $downpayment --- Monthly Payment: ${calculateMortgage(offer, downpayment, interest, term).toString()}",
             style: TextStyle(
               color: Theme.of(context).colorScheme.primary,
               fontSize: 10,
