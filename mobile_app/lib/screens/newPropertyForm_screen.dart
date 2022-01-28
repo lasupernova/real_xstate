f640b82f49c744f84d8f2495b94048aead3f17c7
lasupernova <gabrilasuno@gmail.com>
2022-01-28 14:16:50 -0500

    created screen with Form() for new prtoperty creation

---
+++ mobile_app/lib/screens/newPropertyForm_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NewPropertyForm extends StatefulWidget {
+  static const routeName = "/add-property";
+  NewPropertyForm({Key? key}) : super(key: key);
+
+  @override
+  State<NewPropertyForm> createState() => _NewPropertyFormState();
+}
+
+class _NewPropertyFormState extends State<NewPropertyForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
