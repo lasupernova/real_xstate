ae75ee3b362d9d882e2dee83587cf72c43e39b17
lasupernova <gabrilasuno@gmail.com>
2022-01-28 13:38:09 -0500

    implemented listener and modified ListView() builder to work with refactored code

--- mobile_app/lib/screens/propertiesOverview_screen.dart
+++ mobile_app/lib/screens/propertiesOverview_screen.dart
@@ -1,26 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import '../dummy_data/propertyTileDummy.dart';
+import '../providers/property_list.dart';
+import '../widgets/propertyOverviewTile.dart';
 
 class PropertyOverviewScreen extends StatelessWidget {
   static const routeName = "/properties_overview";
 
-  List<dynamic> _currentTileList =
-      propertyTileDummy; // created for latter TODO (add filter menu, to filter properties based on parameters, e.g. totalIncome etc.)
+  // created for latter TODO (add filter menu, to filter properties based on parameters, e.g. totalIncome etc.)
 
   @override
   Widget build(BuildContext context) {
+    final _properties = Provider.of<PropertyList>(context).fetchProperties;
     return Scaffold(
       appBar: AppBar(
-        title: Text("Properties"),
-        centerTitle: true,
-        actions: <Widget>[
+        // title: Text("Properties"),
+        // centerTitle: true,
+        title: Row(children: [
+          // added IconButtons() previosuly under "actions" to "title", for them to be centered in AppBar
           IconButton(
             onPressed: () => print("Adding Property"),
             icon: Icon(Icons.add_business),
             color: Theme.of(context).colorScheme.onPrimary,
-          )
-        ],
+          ),
+          IconButton(
+              onPressed: () => print("Filtering will be implemented"),
+              icon: Icon(Icons.filter_alt)),
+          IconButton(
+              onPressed: () => print("Sharing/Downloading will be implemented"),
+              icon: Icon(Icons.download)),
+        ]),
       ),
       body: Column(
         children: <Widget>[
@@ -30,10 +39,13 @@ class PropertyOverviewScreen extends StatelessWidget {
               itemBuilder: (BuildContext context, int index) {
                 return Card(
                   elevation: 3,
-                  child: Center(child: _currentTileList[index]),
+                  child: Center(
+                      child: PropertyOverviewTile(
+                    passedProperty: _properties[index],
+                  )),
                 );
               },
-              itemCount: _currentTileList.length,
+              itemCount: _properties.length,
             )),
           ),
         ],
