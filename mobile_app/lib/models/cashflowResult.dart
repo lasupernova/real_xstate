038cdac187c293de50e0971da5d4e30dafe8ce24
lasupernova <gabrilasuno@gmail.com>
2022-02-05 13:29:02 -0500

    added method to calculate total monthly expenses and monthly net operating costs

--- mobile_app/lib/models/cashflowResult.dart
+++ mobile_app/lib/models/cashflowResult.dart
@@ -5,13 +5,19 @@ class CashflowItem {
   // const PropertyItem({Key? key}) : super(key: key);
   String id;
   double mortgage;
-  double propMngmtPerc;
+  double
+      propMgmtPerc; // TODO: group values where sensible (e.g. combine all rent assoc expenses into a list, to nit clutter constructor)
   double vacancyLossPerc;
   double capitalExpPerc;
   double propMgmt;
   double vacancyLoss;
   double capitalExp;
+  double monthlyExpensesReal;
+  double monthlyExpensesHypo;
+  double monthlyNetOpCostsReal;
+  double monthlyNetOpCostsHypo;
   List rents;
+  List costs;
   final double offer;
   final double downpayment;
   final double interest;
@@ -21,13 +27,18 @@ class CashflowItem {
   CashflowItem({
     this.id = "not given",
     this.mortgage = 0,
-    this.propMngmtPerc = 0.09,
+    this.propMgmtPerc = 0.09,
     this.vacancyLossPerc = 0.05,
     this.capitalExpPerc = 0.05,
     this.propMgmt = 0,
     this.vacancyLoss = 0,
     this.capitalExp = 0,
+    this.monthlyExpensesReal = 0,
+    this.monthlyExpensesHypo = 0,
+    this.monthlyNetOpCostsReal = 0,
+    this.monthlyNetOpCostsHypo = 0,
     this.rents = const [],
+    this.costs = const [],
     required this.offer,
     required this.downpayment,
     required this.interest,
@@ -56,14 +67,34 @@ class CashflowItem {
   //   return M;
   // }
 
+  // Calculates expenses that are based on total rental income, namely proeprty management costs,
+  // potential vacancy losses and capital expenditures.
+  // Calculations based on rental income [ rents ] and percentages from rents defined for each of these expenses.
   void rentalAssocExpenses() {
     double totalRents = rents.fold(
         0,
         (previous, current) =>
             previous + current); // sum all values in list together
-    propMgmt = totalRents * propMngmtPerc;
+    propMgmt = totalRents * propMgmtPerc;
     vacancyLoss = totalRents * vacancyLossPerc;
     capitalExp = totalRents * capitalExpPerc;
     return;
+  }
+
+// Calculates total monthly expenses (hypo or 'real'), including:
+// total monthly expeneses [ monthlyExpenses ] (INCLUDES mortgage payments)
+// total monthly net operating costs [ monthlyNetOpCosts ] (EXCLUDES mortgage payments)
+// .
+// Calculations based on rental income [ rents ], recurring non-rental associated costs [ costs ] (utilities, taxes, HOA, insurance...),
+// monthly mortgage [ mortage ]
+  void _totalExpensesMonthly() {
+    double totalCosts = costs.fold(
+        0,
+        (previous, current) =>
+            previous + current); // sum all values in list together
+    monthlyNetOpCostsReal = propMgmt + totalCosts;
+    monthlyNetOpCostsHypo = propMgmt + vacancyLoss + capitalExp + totalCosts;
+    monthlyExpensesReal = monthlyNetOpCostsReal + mortgage;
+    monthlyExpensesHypo = monthlyNetOpCostsHypo + mortgage;
   }
 }
