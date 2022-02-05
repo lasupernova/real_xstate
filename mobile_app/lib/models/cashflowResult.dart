7a2b81ff908774d095d7cfadca2007254d11f5f6
lasupernova <gabrilasuno@gmail.com>
2022-02-05 13:36:17 -0500

    made individual calculation methods private and created method to call all private functions from outside the script all at once

--- mobile_app/lib/models/cashflowResult.dart
+++ mobile_app/lib/models/cashflowResult.dart
@@ -47,30 +47,30 @@ class CashflowItem {
     // ignore: non_constant_identifier_names
     // required this.datetime = DateTime.now(),
   });
+
 // Calculates monthly mortage payment [M] based [ offer - offer made for property ],
 // [ downpayment - already made ], [ interest - interest rate on loan ] and
 // [ term - total time (years) that loan will be running ].
-// The total loan amount [ loanAmount ] iscalculated based on inputs for offer and downpayment
-  // double calculateMortgage(
-  //   double offer, // in $
-  //   double downpayment, //in %
-  //   double interest, // yearly in %
-  //   int term, // in years --> calculation itself takes months, therefore if years are given term needs to be 'translated' into months
-  // ) {
-  //   double loanAmount = offer * (1 - downpayment / 100);
-  //   interest = interest / 100 / 12; // convert to monthly interest rate
-  //   term = term * 12; // convert to months
-  //   double M = loanAmount *
-  //       (interest * pow((1 + interest), term)) /
-  //       (pow((1 + interest), term) - 1);
-  //   mortgage = M;
-  //   return M;
-  // }
+// The total loan amount [ loanAmount ] is calculated based on inputs for offer and downpayment
+  void _calculateMortgage(
+    double offer, // in $
+    double downpayment, //in %
+    double interest, // yearly in %
+    int term, // in years --> calculation itself takes months, therefore if years are given term needs to be 'translated' into months
+  ) {
+    double loanAmount = offer * (1 - downpayment / 100);
+    interest = interest / 100 / 12; // convert to monthly interest rate
+    term = term * 12; // convert to months
+    double M = loanAmount *
+        (interest * pow((1 + interest), term)) /
+        (pow((1 + interest), term) - 1);
+    mortgage = M;
+  }
 
   // Calculates expenses that are based on total rental income, namely proeprty management costs,
   // potential vacancy losses and capital expenditures.
   // Calculations based on rental income [ rents ] and percentages from rents defined for each of these expenses.
-  void rentalAssocExpenses() {
+  void _rentalAssocExpenses() {
     double totalRents = rents.fold(
         0,
         (previous, current) =>
@@ -96,5 +96,11 @@ class CashflowItem {
     monthlyNetOpCostsHypo = propMgmt + vacancyLoss + capitalExp + totalCosts;
     monthlyExpensesReal = monthlyNetOpCostsReal + mortgage;
     monthlyExpensesHypo = monthlyNetOpCostsHypo + mortgage;
+  }
+
+  void calculateCashflow() {
+    _calculateMortgage(offer, downpayment, interest, term);
+    _rentalAssocExpenses();
+    _totalExpensesMonthly();
   }
 }
