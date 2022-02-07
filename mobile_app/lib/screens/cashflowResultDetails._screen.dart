16151deed38242c53a75ace19c4b6e6251adbbdb
lasupernova <gabrilasuno@gmail.com>
2022-02-07 15:08:31 -0500

    added all cashflow info into table

--- mobile_app/lib/screens/cashflowResultDetails._screen.dart
+++ mobile_app/lib/screens/cashflowResultDetails._screen.dart
@@ -55,100 +55,166 @@ class _CfResultDetailsScreenState extends State<CfResultDetailsScreen> {
           child: Center(
             child: Column(
               children: [
-                SingleChildScrollView(
-                  child: Container(
-                      padding: EdgeInsets.symmetric(
-                          vertical: MediaQuery.of(context).size.height * 0.03,
-                          horizontal: MediaQuery.of(context).size.width * 0.03),
-                      alignment: Alignment.center,
-                      child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                Flexible(
-                                  flex: 4,
-                                  child: Text(
-                                    "${CF_item.id}",
-                                    style: TextStyle(fontSize: 17),
-                                  ),
+                Container(
+                    padding: EdgeInsets.symmetric(
+                      vertical: MediaQuery.of(context).size.height * 0.03,
+                      // horizontal: MediaQuery.of(context).size.width * 0.03),
+                    ),
+                    alignment: Alignment.center,
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Flexible(
+                                flex: 4,
+                                child: Text(
+                                  "${CF_item.id}",
+                                  style: TextStyle(fontSize: 17),
                                 ),
-                                Flexible(
-                                  flex: 2,
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        "${DateFormat('yy-MM-dd').format(CF_item.calcDate)}",
-                                      ),
-                                      Text(
-                                          ""), // TODO: find more elegant solution for pushing text to the top
-                                      Text(""),
-                                      Text(""),
-                                      Text(""),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                            // const Spacer(),
-                            Row(children: [
-                              CircleAvatarInfo(
-                                  text1: CF_item.term.toString(),
-                                  text2: "years"),
-                              const Spacer(),
-                              CircleAvatarInfo(
-                                  text1: CF_item.interest.toString(),
-                                  text2: "%"),
-                              const Spacer(),
-                              CircleAvatarInfo(
-                                  text1: CF_item.offer < 10000
-                                      ? "\$${(CF_item.offer).toString().split(".")[0]}"
-                                      : CF_item.offer < 1000000
-                                          ? "\$${(CF_item.offer / 1000).toString()}k"
-                                          : "\$${(CF_item.offer / 1000000).toString()}M",
-                                  text2: "offer"),
-                              const Spacer(),
-                              CircleAvatarInfo(
-                                  text1: "${CF_item.downpayment.toString()} %",
-                                  text2: "down"),
-                            ]),
-                            ListTile(
-                              leading: Icon(Icons.attach_money),
-                              title: Text(
-                                CF_item.mortgage.toStringAsFixed(2),
-                                style: TextStyle(
-                                    color:
-                                        Theme.of(context).colorScheme.primary),
                               ),
-                              subtitle: Text("Monthly Mortgage"),
+                              Flexible(
+                                flex: 2,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "${DateFormat('yy-MM-dd').format(CF_item.calcDate)}",
+                                    ),
+                                    Text(
+                                        ""), // TODO: find more elegant solution for pushing text to the top
+                                    Text(""),
+                                    Text(""),
+                                    Text(""),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          // const Spacer(),
+                          Row(children: [
+                            CircleAvatarInfo(
+                                text1: CF_item.term.toString(), text2: "years"),
+                            const Spacer(),
+                            CircleAvatarInfo(
+                                text1: CF_item.interest.toString(), text2: "%"),
+                            const Spacer(),
+                            CircleAvatarInfo(
+                                text1: CF_item.offer < 10000
+                                    ? "\$${(CF_item.offer).toString().split(".")[0]}"
+                                    : CF_item.offer < 1000000
+                                        ? "\$${(CF_item.offer / 1000).toString()}k"
+                                        : "\$${(CF_item.offer / 1000000).toString()}M",
+                                text2: "offer"),
+                            const Spacer(),
+                            CircleAvatarInfo(
+                                text1: "${CF_item.downpayment.toString()} %",
+                                text2: "down"),
+                          ]),
+                          ListTile(
+                            leading: Icon(Icons.attach_money),
+                            title: Text(
+                              CF_item.mortgage.toStringAsFixed(2),
+                              style: TextStyle(
+                                  color: Theme.of(context).colorScheme.primary),
                             ),
-                            SizedBox(
-                              height: _screenheight * 0.05,
+                            subtitle: Text("Monthly Mortgage"),
+                          ),
+                          SizedBox(
+                            height: _screenheight * 0.05,
+                          ),
+                          SingleChildScrollView(
+                            // TODO: check why overflowing!!
+                            child: Container(
+                              width: _screenwidth * 0.9,
+                              height: _screenheight * 0.2,
+                              child: Table(
+                                children: [
+                                  TableRow(children: [
+                                    Text("          "),
+                                    Text(
+                                      "Real",
+                                      textScaleFactor: 1.5,
+                                    ),
+                                    Text(
+                                      "Hypo",
+                                      textScaleFactor: 1.5,
+                                    ),
+                                  ]),
+                                  TableRow(children: [
+                                    Text("Income"),
+                                    Text(""),
+                                    Text("")
+                                  ]),
+                                  TableRow(children: [
+                                    Text("Net Operating"),
+                                    Text(CF_item.monthlyNetOpIncomeReal
+                                        .toString()),
+                                    Text(CF_item.monthlyNetOpIncomeHypo
+                                        .toString()),
+                                  ]),
+                                  TableRow(children: [
+                                    Text("Expenses"),
+                                    Text(""),
+                                    Text("")
+                                  ]),
+                                  TableRow(children: [
+                                    Text("Net Operating"),
+                                    Text(CF_item.monthlyNetOpCostsReal
+                                        .toString()),
+                                    Text(CF_item.monthlyNetOpCostsHypo
+                                        .toString()),
+                                  ]),
+                                  TableRow(children: [
+                                    Text("Total Monthly (incl. mortgage)"),
+                                    Text(
+                                        CF_item.monthlyExpensesReal.toString()),
+                                    Text(
+                                        CF_item.monthlyExpensesHypo.toString()),
+                                  ]),
+                                  TableRow(children: [
+                                    Text("Cashflow Stats"),
+                                    Text(""),
+                                    Text("")
+                                  ]),
+                                  TableRow(children: [
+                                    Text("Cashflow"),
+                                    Text(
+                                        CF_item.cashflowMonthlyReal.toString()),
+                                    Text(
+                                        CF_item.cashflowMonthlyHypo.toString()),
+                                  ]),
+                                  TableRow(children: [
+                                    Text("coc ROI"),
+                                    Text(CF_item.cocROIReal.toString()),
+                                    Text(CF_item.cocROIHypo.toString()),
+                                  ]),
+                                  TableRow(children: [
+                                    Text("Rent-to-Price ratio"),
+                                    Text(CF_item.rentToPrice.toString()),
+                                    Text(CF_item.rentToPrice.toString()),
+                                  ]),
+                                  TableRow(children: [
+                                    Text("CAP Rate"),
+                                    Text(CF_item.capRateReal.toString()),
+                                    Text(CF_item.capRateHypo.toString()),
+                                  ]),
+                                  TableRow(children: [
+                                    Text("ROI"),
+                                    Text(CF_item.rtiReal.toString()),
+                                    Text(CF_item.rtiHypo.toString()),
+                                  ]),
+                                ],
+                              ),
                             ),
-                            Table(
-                              children: const [
-                                TableRow(children: [
-                                  Text("          "),
-                                  Text(
-                                    "Real",
-                                    textScaleFactor: 1.5,
-                                  ),
-                                  Text(
-                                    "Hypo",
-                                    textScaleFactor: 1.5,
-                                  ),
-                                ]),
-                              ],
-                            ),
-                            IconButton(
-                                onPressed: () {
-                                  print("Added to favorites");
-                                },
-                                icon: Icon(Icons.star_border_outlined))
-                          ])),
-                ),
+                          ),
+                          IconButton(
+                              onPressed: () {
+                                print("Added to favorites");
+                              },
+                              icon: Icon(Icons.star_border_outlined))
+                        ])),
               ],
             ),
           ),
